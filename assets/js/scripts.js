ce4b486e334f318b8b4522f0701be5878be38556
rrmbo <ruben.borer@stud.hslu.ch>
2022-05-06 09:03:37 +0200

    commit

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,24 @@
+// Horizontal Scroll
+
+function scrollHorizontally(e) {
+    e = window.event || e;
+    var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+    var scrollSpeed = 60; // Janky jank <<<<<<<<<<<<<<
+    document.documentElement.scrollLeft -= (delta * scrollSpeed);
+    document.body.scrollLeft -= (delta * scrollSpeed);
+    e.preventDefault();
+}
+
+if (window.addEventListener) {
+    // IE9, Chrome, Safari, Opera
+    window.addEventListener("mousewheel", scrollHorizontally, false);
+    // Firefox
+    window.addEventListener("DOMMouseScroll", scrollHorizontally, false);
+} else {
+    // IE 6/7/8
+    window.attachEvent("onmousewheel", scrollHorizontally);
+}
+
 // Rules
 
 let n = 0;
